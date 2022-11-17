99076172e854d9ee4df955d763c2b0fd3b80fe40
Carl Bergenhem <cbergenhem@gmail.com>
2022-11-17 15:41:55 -0500

    Add Sentry to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
 import styles from './App.module.css';
+import * as Sentry from '@sentry/browser';
+import { BrowserTracing } from '@sentry/tracing';
 
 function App() {
+  Sentry.init({
+    dsn: "https://e02e3d4cc365492ab0efbf817f7748e0@o4504176326279168.ingest.sentry.io/4504176332570624",
+    integrations: [new BrowserTracing()],
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
